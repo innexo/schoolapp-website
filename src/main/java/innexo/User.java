866254f3d0c445732aacbbbea79860443d997333
gpi <gpimpale29@gmail.com>
2019-08-08 07:42:40 -0700

    flask

--- src/main/java/innexo/User.java
+++
@@ -1,9 +0,0 @@
-package innexo;
-
-public class User {
-  public int id;
-  public String name;
-  // not public so it doesn't get serialized by jackson
-  String passwordHash;
-  boolean administrator;
-}
